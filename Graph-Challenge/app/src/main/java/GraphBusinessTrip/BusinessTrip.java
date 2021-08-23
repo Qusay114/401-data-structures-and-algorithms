f3268ee090906b1b83e561d599bbed2be289352c
Qusay114 <eng.qusay015@gmail.com>
2021-08-24 01:22:17 +0300

    code challenge 37 but still missing readme and tests

--- Graph-Challenge/app/src/main/java/GraphBusinessTrip/BusinessTrip.java
+++ Graph-Challenge/app/src/main/java/GraphBusinessTrip/BusinessTrip.java
@@ -1,4 +1,25 @@
 package GraphBusinessTrip;
 
+import Graph.Challenge.Graph;
+import Graph.Challenge.Vertex;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class BusinessTrip {
+
+    public Integer tripCost(Graph graph , List<String> cites){
+        Integer cost = 0 ;
+        for (int i = 0 ; i < cites.size()-1 ; i++){
+            Vertex<String> source = new Vertex<>(cites.get(i));
+            Vertex<String> dest = new Vertex<>(cites.get(i+1));
+            if (graph.getWeightedAdjVertices().get(source).get(dest) != null)
+                 cost += graph.getWeightedAdjVertices().get(source).get(dest) ;
+        }
+
+        return cost ;
+
+
+    }
 }
